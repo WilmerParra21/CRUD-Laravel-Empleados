f61b9f61b0a194fa6829e66bb2c28b50b1a65d5a
Wilmer Parra <wilmerparra2118@gmail.com>
2020-12-04 17:32:40 -0430

    login en proceso

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,10 @@ Route::get('empleado/{id}/editar', [\App\Http\Controllers\EmpleadoController::cl
 Route::put('/empleado/{id}', [\App\Http\Controllers\EmpleadoController::class, 'actualizarEmpleado'])->name('empleado.update');
 
 Route::get('/empleado/{id}/eliminar', [\App\Http\Controllers\EmpleadoController::class, 'eliminarEmpleado'])->name('empleado.delete');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
